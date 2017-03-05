2061139cb57845ba62d5386fc26d709c6a5f61d7
erikmura44 <erikmura44@gmail.com>
2017-03-05 14:22:28 -0700

    components and service complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './components/search/search.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+
+import { routing } from './app.routing';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    NavbarComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/search/search.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { SpotifyService } from '../../services/spotify.service';
+
+@Component({
+  selector: 'search',
+  templateUrl: 'search.component.html'
+})
+export class SearchComponent {
+  searchStr:string;
+
+  constructor(private _spotifyService:SpotifyService){
+
+  }
+
+  searchMusic(){
+    this._spotifyService.searchMusic(this.searchStr).subscribe(res => {
+      console.log(res);
+    });
+  }
+}
