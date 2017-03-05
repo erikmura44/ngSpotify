3d3a843bd6ae33f26b03f997543449e1270ff1df
erikmura44 <erikmura44@gmail.com>
2017-03-05 15:34:18 -0700

    artists and albums displaying

--- src/app/components/album/album.component.ts
+++ src/app/components/album/album.component.ts
@@ -5,13 +5,12 @@ import { Album } from '../../../Album';
 import { SpotifyService } from '../../services/spotify.service';
 
 @Component({
-  selector: 'artist',
-  templateUrl: 'artist.component.html'
+  selector: 'album',
+  templateUrl: 'album.component.html'
 })
-export class ArtistComponent implements OnInit {
+export class AlbumComponent implements OnInit {
   id:string;
-  artist: Artist[];
-  albums: Album[];
+  album: Album[];
 
   constructor(private _spotifyService:SpotifyService, private _route:ActivatedRoute){
 
@@ -21,13 +20,9 @@ export class ArtistComponent implements OnInit {
     this._route.params
       .map(params => params['id'])
       .subscribe((id) => {
-        this._spotifyService.getArtist(id)
-          .subscribe(artist => {
-            this.artist = artist;
-          })
-        this._spotifyService.getAlbums(id)
-          .subscribe(albums => {
-            this.albums = albums.items;
+        this._spotifyService.getAlbum(id)
+          .subscribe(album => {
+            this.album = album;
           })
       });
   }
